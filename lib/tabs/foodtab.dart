03736cef6e68d6baf3a025ee57a0e314bd4628bc
Parama Artha <gededharma18@gmail.com>
2020-03-14 01:32:35 +0700

    version 2.2: Fix Food Tab

--- lib/tabs/foodtab.dart
+++ lib/tabs/foodtab.dart
@@ -11,20 +11,22 @@ class _MyHomePageState extends State<foodtab> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFF21BFBD),
-      body: ListView(
+      body: SafeArea(
+        child: Column(
         children: <Widget>[
           Padding(
-            padding: EdgeInsets.only(left: 40.0, top: 30),
-            child: Row(
+            padding: EdgeInsets.only(top: 30),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                Text('Healthy',
+                Text('Great Food',
                     style: TextStyle(
                         fontFamily: 'Montserrat',
                         color: Colors.white,
                         fontWeight: FontWeight.bold,
                         fontSize: 25.0)),
                 SizedBox(width: 10.0),
-                Text('Food',
+                Text('Around the World',
                     style: TextStyle(
                         fontFamily: 'Montserrat',
                         color: Colors.white,
@@ -34,79 +36,36 @@ class _MyHomePageState extends State<foodtab> {
           ),
           SizedBox(height: 40.0),
           Container(
-            height: MediaQuery.of(context).size.height,
+            height: MediaQuery.of(context).size.height-200,
             decoration: BoxDecoration(
               color: Colors.white,
-              borderRadius: BorderRadius.only(topLeft: Radius.circular(75.0)),
+              borderRadius: BorderRadius.only(topLeft: Radius.circular(45.0)),
             ),
-            child: ListView(
-              primary: false,
-              padding: EdgeInsets.only(left: 25.0, right: 20.0),
-              children: <Widget>[
-                Padding(
-                    padding: EdgeInsets.only(top: 45.0),
-                    child: Container(
-                        height: MediaQuery.of(context).size.height - 300.0,
-                        child: ListView(children: [
-                          _buildFoodItem('assets/plate1.png', 'Salmon bowl', '\$24.00'),
-                          _buildFoodItem('assets/plate2.png', 'Spring bowl', '\$22.00'),
-                          _buildFoodItem('assets/plate6.png', 'Avocado bowl', '\$26.00'),
-                          _buildFoodItem('assets/plate5.png', 'Berry bowl', '\$24.00')
-                        ]))),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    Container(
-                      height: 65.0,
-                      width: 60.0,
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                            color: Colors.grey,
-                            style: BorderStyle.solid,
-                            width: 1.0),
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                      child: Center(
-                        child: Icon(Icons.search, color: Colors.black),
-                      ),
-                    ),
-                    Container(
-                      height: 65.0,
-                      width: 60.0,
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                            color: Colors.grey,
-                            style: BorderStyle.solid,
-                            width: 1.0),
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                      child: Center(
-                        child: Icon(Icons.shopping_basket, color: Colors.black),
-                      ),
-                    ),
-                    Container(
-                      height: 65.0,
-                      width: 120.0,
-                      decoration: BoxDecoration(
-                          border: Border.all(
-                              color: Colors.grey,
-                              style: BorderStyle.solid,
-                              width: 1.0),
-                          borderRadius: BorderRadius.circular(10.0),
-                          color: Color(0xFF1C1428)),
-                      child: Center(
-                          child: Text('Checkout',
-                              style: TextStyle(
-                                  fontFamily: 'Montserrat',
-                                  color: Colors.white,
-                                  fontSize: 15.0))),
-                    )
-                  ],
-                )
-              ],
+            child: Container(
+              child: ListView(
+                primary: false,
+                padding: EdgeInsets.only(left: 25.0, right: 20.0, top: 30.0),
+                children: <Widget>[
+                  _buildFoodItem('assets/plate1.png', 'Salmon bowl', '\$24.00'),
+                  _buildFoodItem('assets/plate2.png', 'Spring bowl', '\$22.00'),
+                  _buildFoodItem('assets/plate6.png', 'Avocado bowl', '\$26.00'),
+                  _buildFoodItem('assets/plate5.png', 'Berry bowl', '\$24.00'),
+                  /*Padding(
+                      padding: EdgeInsets.only(top: 45.0),
+                      child: Container(
+                          height: MediaQuery.of(context).size.height - 300.0,
+                          child: ListView(children: [
+                            _buildFoodItem('assets/plate1.png', 'Salmon bowl', '\$24.00'),
+                            _buildFoodItem('assets/plate2.png', 'Spring bowl', '\$22.00'),
+                            _buildFoodItem('assets/plate6.png', 'Avocado bowl', '\$26.00'),
+                            _buildFoodItem('assets/plate5.png', 'Berry bowl', '\$24.00')
+                          ]))),*/
+                ],
+              ),
             ),
           )
         ],
+        ),
       ),
     );
   }
